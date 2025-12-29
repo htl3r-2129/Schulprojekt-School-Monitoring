e93f5badfdc80ce308c7e36c1186c4f5b14d20b0
Timon Pfeiffer <2148@htl.rennweg.at>
2025-12-29 15:09:47 +0100

    fixed issue where users could create accounts with an email already in use

--- src/Auth.php
+++ src/Auth.php
@@ -10,11 +10,11 @@ class Auth {
     
     public function __construct() {
         $this->db = new DB();
-        $this->users = [
-            // Beispiel-Benutzer TODO: user aus db auslesen
-            // should contain array with user data as string, not user classes (maybe)
-            new User("Max Musterman", "musterman@htl.rennweg.at", "password", $this->db)
-        ];
+//        $this->users = [
+//            // Beispiel-Benutzer TODO: user aus db auslesen
+//            // should contain array with user data as string, not user classes (maybe)
+//            new User("Max Musterman", "musterman@htl.rennweg.at", "password", $this->db)
+//        ];
     }
     
     public function login(string $username, string $password) {
@@ -28,9 +28,16 @@ class Auth {
     }
     
     public function register(string $username, string $email, string $password) {
-        // TODO: check if e-mail is already in use
-        new User($username, $email, $password, $this->db);
-        // TODO: notify frontend so user is forwarded to dashboard
-        return true;
+        $stmt = $this->db->getConn()->prepare("SELECT * FROM user WHERE email = ?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
+        if (!$stmt->fetch()){
+            new User($username, $email, $password, $this->db);
+            // TODO: notify frontend so user is forwarded to dashboard
+            return true;
+        }
+        echo "Error: User with that email already exists.";
+        // TODO: show the error in a more pleasant way
+        return false;
     }
 }
